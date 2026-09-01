f929880b271a0d3c34a8d787db0daaa9d7254cd7
katagaki <68231035+katagaki@users.noreply.github.com>
2026-09-02 07:57:50 +0900

    Initial Commit

---
+++ Plates/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Plates
+//
+//  Created by Justin Xin on 2026/09/02.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Plates/PlatesApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlatesApp.swift
+//  Plates
+//
+//  Created by Justin Xin on 2026/09/02.
+//
+
+import SwiftUI
+
+@main
+struct PlatesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
